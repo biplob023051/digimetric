59209421b7457db9469d2a83fee7af74bd92e77c
Rakib Hasan <rakibhasan880@gmail.com>
2015-04-25 02:56:23 +0600

    unused files remove from git index

--- app/webroot/html/js/newjs.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function(){
-//  $(".add_image").click(function(){
-//     $(".add_img .hover_detail").fadeIn("slow");
-//   });
-//   $(".close_bt").click(function(){
-//     $(".add_img .hover_detail").hide("slow");
-//   });
-  
-// });
